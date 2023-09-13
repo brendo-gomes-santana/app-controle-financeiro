1d8a87da71ccc8d1d88e01630106c55774e23dd7
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-09-13 20:54:42 -0300

    conexão com firebase para cadastrar usuário concluido.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Container,
   Input,
@@ -10,6 +10,7 @@ import {
 } from './styled'
 
 export default function Login({ navigation }) {
+  
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
--- src/pages/NovaConta/index.jsx
+++ src/pages/NovaConta/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import { firebase_auth, firebase_db } from '../../config'
@@ -17,23 +18,29 @@ export default function NovaConta({ navigation }) {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [carregando, setCarregando] = useState(false);
 
   async function handleCadastrarUsuario() {
+    setCarregando(true)
+    await createUserWithEmailAndPassword(firebase_auth, email, senha)
+      .then(async (value) => {
 
-        await createUserWithEmailAndPassword(firebase_auth, email, senha)
-        .then(async( value ) => {
-          
-          await setDoc(doc(firebase_db, 'users', value.user.uid), {
-            id: value.user.uid,
-            nome: nome,
-            email: value.user.email,
-          }).then(() => {
-            console.log('cadastrado com sucesso')
-          }).catch((err)=> {
-            console.log('não foi cadastrado', err)
-          })
+        await setDoc(doc(firebase_db, 'users', value.user.uid), {
+          id: value.user.uid,
+          nome: nome,
+          email: value.user.email,
+        }).then(() => {
+
+          navigation.navigate('Login')
+          setCarregando(false)
+        
+        }).catch((err) => {
+          console.log('não foi cadastrado', err)
+          setCarregando(false)
+        })
       }).catch((err) => {
         console.log(err);
+        setCarregando(false);
       })
 
 
@@ -50,15 +57,19 @@ export default function NovaConta({ navigation }) {
         placeholder='Digite seu nome' />
       <Input
         value={email}
-        onChangeText={t => setEmail(t)} 
+        onChangeText={t => setEmail(t)}
         placeholder='Digite seu email' />
-      <Input 
+      <Input
         value={senha}
-        onChangeText={t => setSenha(t)} 
-        placeholder='Crie sua senha' 
+        onChangeText={t => setSenha(t)}
+        placeholder='Crie sua senha'
         secureTextEntry={true} />
       <Button onPress={() => handleCadastrarUsuario()}>
-        <TextButton>Cadastrar</TextButton>
+        {carregando ? (
+          <ActivityIndicator size={35} color='#fff'/> 
+        ):(
+          <TextButton>Cadastrar</TextButton>
+        )}
       </Button>
       <Link onPress={() => navigation.goBack()}>
         <TextLink>Você já possui conta? Faça seu login</TextLink>
