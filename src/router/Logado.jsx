c58ae613ae5c6e3fcf671832fcfc1f399bd56756
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-09-15 16:11:02 -0300

    ajeitando o menu de baixo, colocando na cor e ajustando tudo

--- src/router/Logado.jsx
+++ src/router/Logado.jsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { Entypo, Ionicons } from '@expo/vector-icons';
+
 import Painel from '../pages/Painel';
 import User from '../pages/User';
 import CriarParcelamento from '../pages/CriarParcelamento';
@@ -10,33 +12,67 @@ const Stack = createStackNavigator();
 
 export default function Logado() {
   return (
-    <BottomTab.Navigator 
+    <BottomTab.Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: {
+          backgroundColor: '#100C33',
+        },
+
+        tabBarInactiveTintColor: '#ddd',
+        tabBarActiveTintColor: '#7A81FF'
+
+        
       }}
     >
-      <BottomTab.Screen name='Painel' component={Home} />
-      <BottomTab.Screen name='Criar Parcelamento' component={CriarParcelamento}/>
-      <BottomTab.Screen name='Configuração' component={User}/>
+      <BottomTab.Screen
+        name='Painel'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="home" size={size} color={color} />
+          }
+        }}
+      />
+      <BottomTab.Screen
+        name='Criar Parcelamento'
+        component={CriarParcelamento}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="md-add-circle" size={size} color={color} />
+          }
+        }}
+      />
+      <BottomTab.Screen
+        name='Configuração'
+        component={User}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="settings" size={size} color={color} />
+          }
+        }}
+      />
     </BottomTab.Navigator>
   )
 }
 
-function Home(){
-  return(
+function Home() {
+  return (
     <Stack.Navigator
-    screenOptions={{
-      headerShown: false
-    }}
+      screenOptions={{
+        headerShown: false
+      }}
     >
-      <Stack.Screen 
-        name='Home' 
+      <Stack.Screen
+        name='Home'
         component={Painel}
-        />
-      <Stack.Screen 
+      />
+      <Stack.Screen
         name='Detalhe'
         component={Detalhe}
-        />
+      />
     </Stack.Navigator>
   )
 }
