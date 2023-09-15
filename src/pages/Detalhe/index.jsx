4ee1d11fcff10c110328a365fe1043ae1fdf9780
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-09-15 14:30:52 -0300

    função de deletar divida feita

--- src/pages/Detalhe/index.jsx
+++ src/pages/Detalhe/index.jsx
@@ -1,20 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { doc, deleteDoc } from 'firebase/firestore';
+import { ActivityIndicator } from 'react-native';
 
-import { Header,  Container, Titulo, Volta} from './styled';
-export default function Detalhe({route, navigation}) {
-    const { data } = route.params;
-    console.log(data)
+import { firebase_db } from '../../config';
+import { 
+      Header, 
+      Container, 
+      Titulo, 
+      Volta, 
+      Deleta, 
+      Base, 
+      BasePagarEAlterar, 
+      Button, 
+      TextoButton,
+      Descricao,
+      Strong
+    } from './styled';
+export default function Detalhe({ route, navigation }) {
+
+  const { data } = route.params;
+  const [carregandoDelete, setCarregandoDelete] = useState(false);
+  const [carregandoPagar, setCarregandoPagar] = useState(false);
+
+  async function handleDelete(){
+    setCarregandoDelete(true);
+    try{
+      const docRef = doc(firebase_db,'dividas',data.id);
+      await deleteDoc(docRef)
+      navigation.push('Home')
+    }catch(err){
+      console.log('Não deu pra deleta', err);
+    }finally{
+      setCarregandoDelete(false);
+    }
+  }
+
+  async function handlePagar(){
+    setCarregandoPagar(true)
+    try{
+
+    }catch(err){
+      console.log('não deu pra atualizar: ', err)
+    }finally{
+      setCarregandoPagar(false)
+    }
+  }
   return (
     <>
       <Header>
         <Volta onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back" size={35} color="#fff" />
+          <Ionicons name="arrow-back" size={35} color="#fff" />
         </Volta>
-        <Titulo>{data.titulo}</Titulo>
+        <Base>
+          <Titulo>{data.titulo}</Titulo>
+          <Deleta onPress={() => handleDelete()}>
+            {carregandoDelete ? (
+              <ActivityIndicator size={30} color='#fff'/>
+            ) : (
+              <Ionicons name="trash-bin-sharp" size={24} color="#fff" />
+            )}
+          </Deleta>
+        </Base>
       </Header>
+
       <Container>
 
+        <BasePagarEAlterar>
+          <Button 
+            color="#8db989"
+          >
+            <TextoButton>Pagar</TextoButton>
+          </Button>
+          <Button
+            color="#B98989"
+          >
+            <TextoButton>Alterar</TextoButton>
+          </Button>
+        </BasePagarEAlterar>
+        <Descricao>
+          <Strong>Quantidade de parcelas: </Strong>{data.quantidade_de_parcelas}
+        </Descricao>
+        <Descricao>
+          <Strong>data de vescimento: </Strong>{data.data}
+        </Descricao>
+        <Descricao>
+          <Strong>Valor: </Strong>{data.valor}
+        </Descricao>
+        <Descricao style={{textAlign: 'center'}}>
+          <Strong>Descricao </Strong>{'\n' + data.descricao}
+        </Descricao>
       </Container>
     </>
   )
--- src/pages/Detalhe/styled.js
+++ src/pages/Detalhe/styled.js
@@ -11,20 +11,56 @@ export const Header = styled.View`
 `;
 
 export const Volta = styled.TouchableOpacity`
-    width: 70px;
+    width: 15%;
     height: 100%;
-    margin-right: 10px;
-
     align-items: center;
     justify-content: center;
-
 `;
+export const Base = styled.View`
+    flex-direction: row;
+    width: 85%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 15px;
+`;
+
 export const Titulo = styled.Text`
     font-size: 20px;
     color: #fff;
     font-weight: bold;
 `;
 
+export const Deleta = styled.TouchableOpacity``;
+
 export const Container = styled.View`
     flex: 1;
+    background-color: #C4C4C4;
+    padding: 10px;
+`;
+export const BasePagarEAlterar = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-bottom: 20px;
+`;
+export const Button = styled.TouchableOpacity`
+    height: 50px;
+    width: 100px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    background-color: ${props => props.color};
+    `;
+export const TextoButton = styled.Text`
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+export const Descricao = styled.Text`
+    font-size: 18px;
+    margin-bottom: 10px;
+`;
+export const Strong = styled.Text`
+    font-weight: bold;
 `;
